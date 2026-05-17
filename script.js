503ba266de59b8e43914639c05897a96ccee1851
Kaviyarasi Raju <kaviyarasiraju79@gmail.com>
2026-05-17 23:08:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function removecolor() {
+    const select = document.getElementById("colorSelect");
+
+    // Remove selected option
+    select.remove(select.selectedIndex);
+}
